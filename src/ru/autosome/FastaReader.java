103c77198286afe566ca1794d7d1c2585161a07e
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2017-06-20 16:26:09 +0300

    sarus now can accept multifasta from stdin using - placeholder for filename

--- src/ru/autosome/FastaReader.java
+++ src/ru/autosome/FastaReader.java
@@ -24,8 +24,16 @@ public class FastaReader implements Iterable<NamedSequence> {
     return new FastaReader(new BufferedReader(new FileReader(file), 10*1024*1024));
   }
 
+  public static FastaReader fromStdin() throws FileNotFoundException {
+    return new FastaReader(new BufferedReader(new InputStreamReader(System.in), 10*1024*1024));
+  }
+
   public static FastaReader fromFile(String filename) throws FileNotFoundException {
-    return fromFile(new File(filename));
+    if (filename.equals("-")) {
+      return fromStdin();
+    } else {
+      return fromFile(new File(filename));
+    }
   }
 
   public static FastaReader fromString(String string) {
