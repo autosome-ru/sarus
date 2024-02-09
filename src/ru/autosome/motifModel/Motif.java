6bbeda24457c802590514c13bbab3c0cb8c23a7c
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2024-02-09 21:31:19 +0300

    fix Motif generic-typing

--- src/ru/autosome/motifModel/Motif.java
+++ src/ru/autosome/motifModel/Motif.java
@@ -1,7 +1,7 @@
 package ru.autosome.motifModel;
 
 //matrix: first string - name; row - position, column - nucleotide
-public interface Motif<T extends Motif> {
+public interface Motif<T extends Motif<T>> {
     T revcomp();
     int motif_length(); // length of matched sequence in nucleotides
 }
